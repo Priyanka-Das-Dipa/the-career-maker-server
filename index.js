c414a578919d93c5dd9f469a2bd1107f3ca92253
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-11-08 15:04:33 +0600

    jwt token add

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken')
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: [
-    'http://localhost:5173'
-  ],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 // careerMaker
 // rVkQNLEkFQafGLty
 
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mfd0sli.mongodb.net/?retryWrites=true&w=majority`;
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mfd0sli.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,65 +32,101 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middlewares
+
+const logger = (req, res, next) => {
+  console.log("Lo information", req.method, req.url);
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookie?.token;
+  console.log("token from middleware", token);
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+  
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const serviceCollection = client.db('careerMaker').collection('services');
-    const bookingCollection = client.db('careerMaker').collection('bookings');
+    const serviceCollection = client.db("careerMaker").collection("services");
+    const bookingCollection = client.db("careerMaker").collection("bookings");
 
     // token
-    app.post('/jwt', async(req, res)=>{
-       const user = req.body;
-       console.log('user for token', user)
-       const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, {expiresIn: '1h'})
-
-       res.cookie('token', token, {
-        httpOnly: true,
-        secure: true,
-
-       })
-       .send({success: true})
-    })
-
-    app.post('/logout', async(req, res)=>{
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      console.log('logging Out', user)
-      res.clearCookie('token', {maxAge: 0}.send({success: true}))
-    })
+      console.log("user for token", user);
+      const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, {
+        expiresIn: "1h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("logging Out", user);
+      res.clearCookie("token", { maxAge: 0 }.send({ success: true }));
+    });
 
     // server apis
-    app.get('/services', async(req, res) =>{
-        const cursor = serviceCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.post('/services', async(req, res)=>{
-      const newService = req.body
-      console.log(newService)
-      const result = await serviceCollection.insertOne(newService)
-      res.send(result)
-    })
+    app.post("/services", async (req, res) => {
+      const newService = req.body;
+      console.log(newService);
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
 
-    app.get('/services/:id', async(req, res) =>{
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id) };
-      const result = await serviceCollection.findOne(query)
-      res.send(result)
-    })
-    app.get('/bookings', async(req, res)=>{
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+    // booking related api
+    app.get("/bookings", logger, verifyToken, async (req, res) => {
+      console.log(req.query.email);
+      console.log("cookie", req.cookies);
+      console.log("token user ingo", req.user);
+      if(req.user.email !== req.query.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
       const cursor = bookingCollection.find();
-        const result = await cursor.toArray()
-        res.send(result)
-    })
-    app.post('/bookings', async(req, res)=>{
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.post("/bookings", async (req, res) => {
       const newBooking = req.body;
       console.log(newBooking);
-      const result = await bookingCollection.insertOne(newBooking)
-      res.send(result)
-    })
-
+      const result = await bookingCollection.insertOne(newBooking);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
